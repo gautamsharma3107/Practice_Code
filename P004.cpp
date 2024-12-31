82624161fa83ce1cae154bc6a65befbd63ca1e34
gautamsharma3107 <gautamsharma3107@gmail.com>
2025-01-01 02:09:53 +0530

    new changes

---
+++ P004.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string arr[10] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+    string* ptr;
+    ptr = &arr[0];
+    int m,n;
+    cin >>m>> n;
+
+    for(int i = m; i <=n; i++)
+    {
+        if(i>=1&&i<=9)
+        {
+            cout<<ptr[i]<<endl;
+        }
+        else if(i>9)
+        {
+            if(i%2==0)
+            {
+                cout<<"even"<<endl;
+
+            }
+            else
+            {
+                cout<<"odd"<<endl;
+            }
+        }
+    }
+    return 0;
+}
