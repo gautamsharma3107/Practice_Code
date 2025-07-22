873c5756e263668609847cb23413b0ea044138b2
Gautam Sharma <gautamsharma3107@gmail.com>
2025-07-22 22:47:28 +0530

    Day 2

---
+++ Code/TLE/TLE_Level1/Module_3/630A - Again Twenty Five!.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/problemset/problem/630/A
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+typedef unsigned int ui;
+typedef signed int si;
+typedef long long int lli;
+typedef long double ld;
+
+int out(lli n)
+{
+    cin>>n;
+    if(n>=2)
+    {cout<<"25";} 
+    // 5*5 til 5^n give last two digits as 35 only
+    return 0;
+}
+
+int main()
+{
+    lli n;
+    out(n);
+    return 0;
+
+}
--- Program.cpp
+++ Program.cpp
@@ -1,49 +1,46 @@
-//https://atcoder.jp/contests/abc305/tasks/abc305_a
 
 
 #include <iostream>
 #include <bits/stdc++.h>
 #include <algorithm>
+#include <string>
 using namespace std;
+typedef unsigned int ui;
+typedef signed int si;
+typedef long long int lli;
+typedef long double ld;
+
+void rotate(ui n)
+{
+    ui cube[n][n];
+    for(ui i=0; i<n; i++)
+   {
+    for(ui j=0; j<n; j++)
+    {
+        cin>>cube[i][j];
+    }
+   }
+   
+   
+   for(ui i=0; i<n; i++)
+   {
+    for(ui j=0; j<n; j++)
+    {
+        cout<<cube[i][j]<<" ";  
+    }
+    cout<<endl;  
+   }
+     return;
+   
+    
+    
+}
 
 int main()
 {
-    int t;
-    cin>>t;
-    
-    for(int i=0;i<t;i++)
-    {
-        int n;
-        cin>>n;
-        int p[n],s[n];
-        bool result = true;
-        for(int j=0;j<n;j++) cin>>p[j];
-        for(int j=0;j<n;j++) cin>>s[j];
-        std::sort(p, p + n);
-        std::sort(s, s + n);    
-        
-        if(p[0]==s[0])
-        {
-            for(int k=1;k<n;k++)
-            {
-                if((p[k]%p[0]!=0) || (s[k]%s[0]!=0))
-                {
-                    result = false;
-                    break; // Exit immediately when condition fails
-                }
-                // Don't set result = true here, it's already true
-            }
-        }
-        else
-        {
-            result = false; // First elements don't match
-        }
+    ui n;
+    cin>>n;
+    //int cube[n][n];
+    rotate(n);
 
-        if(result) 
-        {cout<<"yes"<<endl;}
-        else 
-        {cout<<"No"<<endl;}
-    }
-
-     return 0;
 }
